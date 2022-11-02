0c41ddbefe1f5e3730e6e32f43bfb948fb88a5ab
Ayush Gupta <ayugupta2506@gmail.com>
2022-11-03 02:23:28 +0530

    Validation Commit

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.utils.main_util import read_yaml_file
+from sensor.constant.training_pipeline import SCHEMA_FILE_PATH
+import os
 
 
 if __name__=='__main__':
-    mongodb_client = MongoDBClient()
-    print(mongodb_client.database.list_collection_names())
+    #mongodb_client = MongoDBClient()
+    #print(mongodb_client.database.list_collection_names())
     #print(MONGODB_URL_KEY)
-    training_pipeline = TrainPipeline()
-    training_pipeline.run_pipeline()
+    #training_pipeline = TrainPipeline()
+    #training_pipeline.run_pipeline()
+    print("Hello World")
